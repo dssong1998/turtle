1c9bcff3dacacc059f78e913c3599be919b29b07
bigstone <dssong019980106@gmail.com>
2021-07-05 22:22:44 +0900

    remove annotation

--- src/GA.js
+++ src/GA.js
@@ -3,30 +3,30 @@ import { Helmet } from "react-helmet-async";
 
 const GAHoc = ({ children }) => {
   useEffect(() => {
-    // if (process.env.NODE_ENV === "production") {
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l !== "dataLayer" ? "&l=" + l : "";
-      j.async = true;
-      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, "script", "dataLayer", "GTM-WHBKXRV");
-    window.dataLayer = window.dataLayer || [];
-    // eslint-disable-next-line
-    function gtag() {
-      window.dataLayer.push(arguments);
+    if (process.env.NODE_ENV === "production") {
+      (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l !== "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", "GTM-WHBKXRV");
+      window.dataLayer = window.dataLayer || [];
+      // eslint-disable-next-line
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-BPG83F1RDZ", {
+        page_location: window.location.href,
+        page_path: window.location.pathname,
+        page_title: window.document.title,
+        state: window.history?.state,
+      });
     }
-    gtag("js", new Date());
-    gtag("config", "G-BPG83F1RDZ", {
-      page_location: window.location.href,
-      page_path: window.location.pathname,
-      page_title: window.document.title,
-      state: window.history?.state,
-    });
-    // }
   }, []);
   return (
     <>
