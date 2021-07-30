dc102447d6d8a15bc9adf52b5832e82a6eb76081
bigstone <dssong019980106@gmail.com>
2021-07-30 21:19:42 +0900

    master backup

--- src/apollo.js
+++ src/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 
 const httpLink = createHttpLink({
-  uri: "https://turtle-backend.herokuapp.com/graphql",
+  uri: "https://192.168.35.213:000/graphql",
 });
 const onErrorLink = onError((e) => {
   console.log(e);
